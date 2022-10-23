ed03417517b96bc3a5bb3d5d188ed7385bdb53c3
febrianto <febriantomanangi@gmail.com>
2022-10-23 15:47:37 +0800

    febrianto - add detail kategory hutan

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 const url = 'https://6352c8eaa9f3f34c3748e117.mockapi.io';
 
 const category = document.getElementById('txt-item')
+const detailHutan = document.getElementById('detail')
+const detailPerairan = document.getElementById('detail-perairan')
 
 async function getMockApi(url) {
 
@@ -14,7 +16,7 @@ async function getMockApi(url) {
 getMockApi(`${url}/informasi`)
 .then(function (result) {
     category.innerHTML = '';
-    result.forEach(function (informasi) {
+    result.slice(0, 4).forEach(function (informasi) {
         category.innerHTML += `<div class="col-sm-3 " >
         <div class="category-item">
             <img src="${informasi.img}" width="100px" alt="">
@@ -30,3 +32,36 @@ getMockApi(`${url}/informasi`)
         `
     })
 })
+
+
+getMockApi(`${url}/informasi`)
+.then(function (result) {
+    detailHutan.innerHTML ='';
+    result.slice(4, 5).forEach(function (informasi) {
+        detailHutan.innerHTML += `
+                <div class="col-sm-6 ">
+                    <div class="detail-item">
+                        <h3>${informasi.title}</h3>
+                        <p>${informasi.content}</p>
+                    </div>
+                </div>
+                <div class="col-sm-6 ">
+                    <div class="detail-item">
+                        <img src="${informasi.img}" width="700px" alt="">
+                    </div>
+                </div>
+                <style> .detail-item img{    
+                    border-radius: 70px;
+                    
+                }
+                @media only screen and (max-width: 900px) {
+                    display: inline;
+                }
+                </style>
+
+        `
+        
+    })
+}) 
+
+
