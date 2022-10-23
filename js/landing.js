0008e5e7ac4f28b0c53123cf7ff0a8ebc11ab594
lisa0104 <lisaliza0104@gmail.com>
2022-10-23 09:59:48 +0700

    add registerlogin tes
    coba cek gengs

---
+++ js/landing.js
@@ -0,0 +1,25 @@
+const penanda = document.getElementById('penanda');
+const signInNav = document.getElementById('signInNav');
+const signUpNav = document.getElementById('signUpNav');
+const signOutNav = document.getElementById('signOutNav');
+
+const buttonSignOut = document.getElementById('buttonSignOut');
+
+const user_id_localStorage = localStorage.getItem('user_id'); // user sudah login
+
+if (user_id_localStorage) {
+  penanda.innerText = "kamu sudah login";
+  signInNav.style.display = 'none';
+  signUpNav.style.display = 'none';
+} else {
+  penanda.innerText = "kamu belum login";
+  signOutNav.style.display = 'none';
+}
+
+buttonSignOut.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  localStorage.removeItem('user_id');
+
+  window.location = 'landing.html';
+});
