aea9719c64eb1ffdeb34c700fe0efa7599ddbe42
MOHAMAD FEBRIANTO MANANGI <91535685+GLynn6@users.noreply.github.com>
2022-10-25 01:06:39 +0800

    Febrianto dampak (#8)
    * feberianto - add category dampak page
    
    * febrianto - add dampak footer
    
    * febrianto - add detail kategory hutan
    
    * febrianto - add detail kategory pangan
    
    * febrianto - add detail kategory perairan
    
    * febrianto - add detail kategory kesehatan
    
    * febrianto - rename folder

---
+++ script/script.js
@@ -0,0 +1,195 @@
+const url = 'https://6352c8eaa9f3f34c3748e117.mockapi.io';
+
+const category = document.getElementById('txt-item')
+const detailHutan = document.getElementById('detail')
+const detailPangan = document.getElementById('detail-pangan')
+const detailPerairan = document.getElementById('detail-perairan')
+const detailKesehatan = document.getElementById('detail-kesehatan')
+
+async function getMockApi(url) {
+
+    const respond = await fetch(url)
+    const result = respond.json();
+
+    return result;
+    
+    
+}
+getMockApi(`${url}/informasi`)
+.then(function (result) {
+    category.innerHTML = '';
+    result.slice(0, 4).forEach(function (informasi) {
+        category.innerHTML += `<div class="col-sm-3 " >
+        <div class="category-item">
+            <img src="${informasi.img}" width="100px" alt="">
+        </div>
+        <div class="category-item">
+            <h5>${informasi.title}</h5>
+        </div>
+        <div class="category-item">
+            <p>${informasi.content}</p>
+        </div>
+    </div>
+
+        `
+    })
+})
+
+
+getMockApi(`${url}/informasi`)
+.then(function (result) {
+    detailHutan.innerHTML ='';
+    result.slice(4, 5).forEach(function (informasi) {
+        detailHutan.innerHTML += `
+                <div class="col-sm-6 ">
+                    <div class="detail-item">
+                        <h3>${informasi.title}</h3>
+                        <p>${informasi.content}</p>
+                    </div>
+                </div>
+                <div class="col-sm-6 ">
+                    <div class="detail-item">
+                        <img src="${informasi.img}" width="600px" alt="">
+                    </div>
+                </div>
+                <style> .detail-item img{    
+                    border-radius: 50px;
+                    
+                }
+                @media only screen and (max-width: 900px) {
+                    .detail-item img{
+                        width: 100%;
+                    }
+                    
+                }
+                </style>
+
+        `
+        
+    })
+}) 
+
+getMockApi(`${url}/informasi`)
+.then(function (result) {
+    detailPangan.innerHTML ='';
+    result.slice(5, 6).forEach(function (informasi) {
+        detailPangan.innerHTML += `
+      
+            <div class="col-sm-6 ">
+                <div class ="detail-item-pangan">
+                    <img src="${informasi.img}"  width="550px" height="450px" alt="">
+                </div>
+            </div>
+            <div class="col-sm-6 ">
+                <div class="detail-item-pangan"> 
+                    <h3>${informasi.title}</h3>
+                    <p>${informasi.content}</p>
+                </div>
+            </div>
+            
+            <style> .detail-item-pangan img{    
+                border-radius: 50px;
+                
+            }
+            @media only screen and (max-width: 900px) {
+                .detail-item-pangan img{
+                    width: 100%;
+
+                }
+                
+                display: inline;
+            }
+            </style>
+
+        </div>
+        </div>
+
+        `
+        
+    })
+}) 
+
+getMockApi(`${url}/informasi`)
+.then(function (result) {
+    detailPerairan.innerHTML ='';
+    result.slice(6, 7).forEach(function (informasi) {
+        detailPerairan.innerHTML += `
+                <div class="col-sm-6 ">
+                    <div class ="detail-item-perairan">
+                        <h3>${informasi.title}</h3>
+                        <p>${informasi.content}</p>
+                    </div>
+                </div>
+                <div class="col-sm-6 ">
+                    <div class="detail-item-perairan">
+                        <img src="${informasi.img}" width="600px" alt="">
+                    </div>
+                </div>
+
+                <style> .detail-item-perairan img{    
+                    border-radius: 50px;
+
+                    
+                    
+                }
+
+                @media only screen and (max-width: 900px) {
+                    .detail-item-perairan img{
+                        width: 100%;
+    
+                    }
+                    
+                    display: inline;
+                }
+                </style>
+
+        `
+        
+    })
+}) 
+
+
+getMockApi(`${url}/informasi`)
+.then(function (result) {
+    detailKesehatan.innerHTML ='';
+    result.slice(7, 8).forEach(function (informasi) {
+        detailKesehatan.innerHTML += `
+        <div class="col-sm-6 ">
+            <div class ="detail-item-kesehatan">
+                <img src="${informasi.img}" width="550px" alt="">
+            </div>
+        </div>
+        <div class="col-sm-6 ">
+            <div class="detail-item-kesehatan">
+                <h3>${informasi.title}</h3>
+                <p>${informasi.content}</p>
+            </div>
+        </div>
+
+
+        <style> .detail-item-kesehatan img{    
+            border-radius: 50px;
+
+            
+            
+        }
+
+        @media only screen and (max-width: 900px) {
+            .detail-item-kesehatan img{
+                width: 100%;
+
+            }
+            
+            
+        }
+    
+        </style>
+
+        `
+        
+    })
+}) 
+
+
+
+
